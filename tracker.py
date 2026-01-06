81dfac6ce08b44f49b68c0a15573c691123b918d
Jenushan <jenushan349@gmail.com>
2026-01-06 13:51:45 -0500

    Connect tracker to database and add product creation through API

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@ from database import engine
 from models import Product, PriceRecord 
 from scraper import get_price 
 import datetime 
+import time
 
 def update_prices(): 
   with Session(engine) as session: 
@@ -11,24 +12,35 @@ def update_prices():
     print("Starting updates at ", datetime.datetime.now())
     
     for product in products: 
-      print("Checking: ", product.name)
-      current_price = get_price(product.url)
+      try: 
+        print("Checking: ", product.name)
+        current_price = get_price(product.url)
 
-      if current_price: 
-        new_record = PriceRecord(
-          price = current_price, 
-          store_name = "Amazon", 
-          product_id = product.id
-        )
-        session.add(new_record)
+        if current_price: 
+          new_record = PriceRecord(
+            price = current_price, 
+            store_name = "Amazon", 
+            product_id = product.id
+          )
+          session.add(new_record)
 
-        if current_price <= product.target_price: 
-          print("Alert: ", product.name, "is now $", current_price)
-        else: 
-          print("No deal yet. Current price: $", current_price)
-    
-    session.commit()
-    print("Update finished")
+          if current_price <= product.target_price: 
+            print("Alert: ", product.name, "is now $", current_price)
+          else: 
+            print("No deal yet. Current price: $", current_price)
+      except Exception as error: 
+        print("Error checking ", product.name, ":", error)
+        continue 
+
+      session.commit()
+      print("Update finished")
+
 
 if __name__ == "__main__":
-  update_prices()
+  while True: 
+    try:
+      update_prices()
+    except Exception as error: 
+      print("Error in loop", error)
+      print("Waiti ng 6 hours for the next check")
+      time.sleep(21600)
